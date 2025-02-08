1b1f6cdf85fd58a83e49dedbabd49e47321a571c
Sourav Shetye <143427277+Sourav0211@users.noreply.github.com>
2025-02-07 19:02:02 -0500

    Fixed bug

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -17,6 +17,11 @@ import { AspectRatioKey, debounce, deepMergeObjects } from '@/lib/utils';
 import { Button } from '../ui/button';
 import MediaUploader from './MediaUploader';
 import TransformedImage from './TransformedImage';
+import { updateCredits } from '@/lib/actions/user.actions';
+import { getCldImageUrl } from 'next-cloudinary';
+import { addImage, updateImage } from '@/lib/actions/image.actions';
+import { useRouter } from 'next/navigation';
+
 
 export const formSchema = z.object({
   title: z.string(),
@@ -47,6 +52,8 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance,co
   const [transformationConfig , setTransformationConfig] = useState(config);
   const [isPending, starttransition] = useTransition()
 
+  const router = useRouter();
+
   const initialValues = data && action === 'Update' ? {
     title: data?.title,
     aspectRatio: data?.aspectRatio,
@@ -59,8 +66,79 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance,co
     resolver: zodResolver(formSchema),
     defaultValues: initialValues
   })
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true)
+
+    if(data|| image)
+    {
+      const transformationUrl = getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig
+      })
+
+      const imageData = {
+        title: values.title,
+        publicId: image?.publicId,
+        transformationType: type,
+        width: image?.width,
+        height: image?.height,
+        config: transformationConfig,
+        secureURL: image?.secureUrl,
+        transformationURL: transformationUrl,
+        aspectRatio: values.aspectRatio,
+        prompt: values.prompt,
+        color: values.color,
+
+      }
+
+      if(action === 'Add')
+      {
+        try {
+          const newImage = await addImage({
+            image: imageData,
+            userId,
+            path:'/'
+
+          })
+
+          if(newImage){
+            form.reset();
+            setImage(data);
+            router.push(`/transformations/${newImage._id}`)
+          }
+        } catch (error) {
+          console.log(error)
+          
+        }
+      }
+
+      if(action === 'Update')
+      {
+        try {
+          const updatedImage = await updateImage({
+            image: {
+              ...imageData,
+              _id: data._id
+            },
+            userId,
+            path:`/transformations/${data._id}`
+
+          })
+
+          if(updatedImage){
+            router.push(`/transformations/${updatedImage._id}`)
+          }
+        } catch (error) {
+          console.log(error)
+          
+        }
+      }
+    }
+
+    setIsSubmitting(false);
+
   }
 
   const onSelectFieldHandler = (value: string, 
@@ -112,7 +190,7 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance,co
     setNewTransformation(null);
 
     startTransition(async () => {
-      // await updateCredits(userId,creditFee)
+      await updateCredits(userId,-1)
     })
   }
   
--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -21,23 +21,22 @@ export interface IImage extends Document{
     updatedAt?: Date;
   }
 
-const ImageSchema = new Schema({
-title: {type:String, required: true},
-transformationType: {type: String, required: true},
-publicId: {type: String, required: true},
-secureUrl: {type: URL, required: true},
-width: {type: Number},
-height: {type: Number},
-config: {type: Object},
-transformationUrl: {type: URL},
-aspectRation: {type: String},
-color: {type: String}, 
-prompt: {type: String},
-author: {type: Schema.Types.ObjectId, ref:'User'},
-createdAt: {type: Date, default:Date.now},
-updatedAt: {type: Date, default: Date.now}
-
-});
+  const ImageSchema = new Schema({
+    title: { type: String, required: true },
+    transformationType: { type: String, required: true },
+    publicId: { type: String, required: true },
+    secureURL: { type: String, required: true },
+    width: { type: Number },
+    height: { type: Number },
+    config: { type: Object },
+    transformationUrl: { type: String },
+    aspectRatio: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
 
 const Image = models?.Image || model('Image', ImageSchema);
 
